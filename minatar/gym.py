d88411c1ea8aa5e6dfdf0417b2d0ab2f3279f20d
Salih, Safin (VW America) <safin.salih@vw.com>
2025-02-22 05:06:51 -0800

    changes

--- minatar/gym.py
+++ minatar/gym.py
@@ -1,28 +1,21 @@
 # Adapted from https://github.com/qlan3/gym-games
-import numpy as np
-import gymnasium as gym
-from gymnasium import spaces
-from gymnasium.envs.registration import register
-
-try:
-    import seaborn as sns
-except:
-    import logging
-    logging.warning("Cannot import seaborn."
-        "Will not be able to train from pixel observations.")
+import gym
+from gym import spaces
+from gym.envs import register
 
 from minatar import Environment
 
 
 class BaseEnv(gym.Env):
-    metadata = {"render_modes": ["human", "array", "rgb_array"]}
+    # metadata = {"render.modes": ["human", "array"]}
+    metadata = {"render_modes": ["human", "array"]}
 
-    def __init__(self, game, render_mode=None, display_time=50,
-                use_minimal_action_set=False, **kwargs):
-        self.render_mode = render_mode
+    def __init__(self, game, display_time=50, use_minimal_action_set=False, **kwargs):
+        self.game_name = game
         self.display_time = display_time
 
-        self.game = Environment(env_name=game, **kwargs)
+        self.game_kwargs = kwargs
+        self.seed()
 
         if use_minimal_action_set:
             self.action_set = self.game.minimal_action_set()
@@ -31,48 +24,31 @@ class BaseEnv(gym.Env):
 
         self.action_space = spaces.Discrete(len(self.action_set))
         self.observation_space = spaces.Box(
-            0, 1, shape=self.game.state_shape(), dtype=np.uint8
+            0.0, 1.0, shape=self.game.state_shape(), dtype=bool
         )
 
     def step(self, action):
         action = self.action_set[action]
         reward, done = self.game.act(action)
-        if self.render_mode == "human":
-            self.render()
-        return self.game.state(), reward, done, False, {}
+        return self.game.state(), reward, done, {}
+
+    def reset(self):
+        self.game.reset()
+        return self.game.state()
 
     def seed(self, seed=None):
-        self.game.seed(seed)
+        self.game = Environment(
+            env_name=self.game_name,
+            random_seed=seed,
+            **self.game_kwargs
+        )
+        return seed
 
-    def reset(self, seed=None, options=None):
-        if seed is not None:
-            self.seed(seed)
-        self.game.reset()
-        if self.render_mode == "human":
-            self.render()
-        return self.game.state(), {}
-
-    def render(self):
-        if self.render_mode is None:
-            gym.logger.warn(
-                "You are calling render method without specifying any render mode. "
-                "You can specify the render_mode at initialization, "
-                f'e.g. gym("{self.spec.id}", render_mode="rgb_array")'
-            )
-            return
-        if self.render_mode == "array":
+    def render(self, mode="human"):
+        if mode == "array":
             return self.game.state()
-        elif self.render_mode == "human":
+        elif mode == "human":
             self.game.display_state(self.display_time)
-        elif self.render_mode == "rgb_array": # use the same color palette of Environment.display_state
-            state = self.game.state()
-            n_channels = state.shape[-1]
-            cmap = sns.color_palette("cubehelix", n_channels)
-            cmap.insert(0, (0,0,0))
-            numerical_state = np.amax(
-                state * np.reshape(np.arange(n_channels) + 1, (1,1,-1)), 2)
-            rgb_array = np.stack(cmap)[numerical_state]
-            return rgb_array
 
     def close(self):
         if self.game.visualized:
@@ -84,12 +60,12 @@ def register_envs():
     for game in ["asterix", "breakout", "freeway", "seaquest", "space_invaders"]:
         name = game.title().replace('_', '')
         register(
-            id="MinAtar/{}-v0".format(name),
+            id="{}-v0".format(name),
             entry_point="minatar.gym:BaseEnv",
-            kwargs=dict(game=game, use_minimal_action_set=False),
+            kwargs=dict(game=game, display_time=50, use_minimal_action_set=False),
         )
         register(
-            id="MinAtar/{}-v1".format(name),
+            id="{}-v1".format(name),
             entry_point="minatar.gym:BaseEnv",
-            kwargs=dict(game=game, use_minimal_action_set=True),
+            kwargs=dict(game=game, display_time=50, use_minimal_action_set=True),
         )
